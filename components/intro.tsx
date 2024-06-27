db1f7e536c81a9cd839ec754ec878ac6497fbc6f
DoneWithWork <fingfingsavage@gmail.com>
2024-06-28 00:27:15 +0800

    escape entity fixed in intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -48,7 +48,7 @@ export default function Intro() {
           duration: 0.7,
         }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Ricardo.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
